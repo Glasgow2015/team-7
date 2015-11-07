8819e65a70a493e17bff4a05b074d1878394ac3c
zoegerolemou <2023949g@student.gla.ac.uk>
2015-11-07 16:56:25 +0000

    Created the Views

--- pcs/views.py
+++ pcs/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+	return HttpResponse("Index")
+
+def support(request):
+	return HttpResponse("Support")
+
+def statistics(request):
+	return HttpResponse("Statistics")
+
+def symptoms(request):
+	return HttpResponse("Symptoms")
+
+def getinvolved(request):
+	return HttpResponse("Get Involved")
+
+def sharedstories(request):
+	return HttpResponse("Shared Stories")
+
+def login(request):
+	return HttpResponse("Login")
