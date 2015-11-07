b4276cc0a051046ae36ace848c96e63f8b4908e2
zoegerolemou <2023949g@student.gla.ac.uk>
2015-11-07 18:25:41 +0000

    Fixed some inconsistencies in the Views file

--- pcs/views.py
+++ pcs/views.py
@@ -1,44 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.shortcuts import render_to_response
 
 def index(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/index.html', context_dict)
 
 def support(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/support.html', context_dict)
 
 def statistics(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/statistics.html', context_dict)
 
 def symptoms(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/symptoms.html', context_dict)
 
 def getinvolved(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/getinvolved.html', context_dict)
 
 def sharedstories(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/sharedstories.html', context_dict)
 
 def login(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/login.html', context_dict)
 
 def about(request):
-	context = RequestContext(request)
 	context_dict = {'boldmessage': "Bold Font"}
 	return render(request, 'pcs/about.html', context_dict)
